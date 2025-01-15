34023c9e916ce12642223a5daa5e6226b357ed44
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2025-01-15 08:42:42 +0000

    🐛 Fixed build

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,34 @@
-module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript',
-    ],
+module.exports = (api) => {
+    const isESM = api.env('esm');
+
+    return {
+        presets: [
+            [
+                '@babel/preset-env',
+                {
+                    modules: isESM ? false : 'cjs',
+                    targets: {
+                        node: 'current',
+                        browsers: '>0.2%, not dead, not op_mini all'
+                    }
+                }
+            ],
+            '@babel/preset-typescript',
+            '@babel/preset-react'
+        ],
+        plugins: [
+            '@babel/plugin-transform-runtime',
+            // Add this custom plugin
+            {
+                visitor: {
+                    ImportDeclaration(path) {
+                        const source = path.node.source;
+                        if (source.value.endsWith('.scss')) {
+                            source.value = source.value.replace(/\.scss$/, '.css');
+                        }
+                    }
+                }
+            }
+        ]
+    };
 };
--- webpack.common.config.js
+++ webpack.common.config.js
@@ -1,27 +1,38 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
-  entry: './src/index.tsx',
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-  },
+    entry: './src/index.tsx',
+    externals: [nodeExternals()],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ],
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: '[name].css'
+            })
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
 };
