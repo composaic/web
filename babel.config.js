2e0d47f331bf8878f602a294a5e6a6c49e1d179f
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2025-02-18 07:22:12 +0000

    🚧 Decorator support / build optimisation

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,24 @@
 module.exports = (api) => {
-    const isESM = api.env('esm');
+  const isESM = api.env('esm');
 
-    return {
-        presets: [
-            [
-                '@babel/preset-env',
-                {
-                    modules: isESM ? false : 'cjs',
-                    targets: {
-                        node: 'current',
-                        browsers: '>0.2%, not dead, not op_mini all'
-                    }
-                }
-            ],
-            '@babel/preset-typescript',
-            '@babel/preset-react'
-        ],
-        plugins: [
-            '@babel/plugin-transform-runtime'
-        ]
-    };
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: isESM ? false : 'cjs',
+          targets: {
+            node: 'current',
+            browsers: '>0.2%, not dead, not op_mini all',
+          },
+          debug: true,
+        },
+      ],
+      '@babel/preset-typescript',
+      '@babel/preset-react',
+    ],
+    plugins: ['@babel/plugin-transform-runtime'],
+    sourceMaps: true,
+    retainLines: true,
+  };
 };
