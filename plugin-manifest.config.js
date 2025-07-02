b1c64202e2f0e599b7b6f2d5efdee5b84531b087
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2025-07-02 08:08:51 +0100

    🔧 Support for forced manifest generation

--- plugin-manifest.config.js
+++ plugin-manifest.config.js
@@ -1,22 +1,20 @@
 /** @type {import('@composaic/core/dist/types/plugin-system/config-types').PluginManifestConfig} */
 module.exports = {
-    plugins: [
-        {
-            type: 'system',
-            source: 'src/plugins/navbar/index.ts',
-            output: 'src/plugins/navbar/navbar-plugin.json',
-        },
-        {
-            type: 'system',
-            source: 'src/plugins/views/index.ts',
-            output: 'src/plugins/views/views-plugin.json',
-        }
-    ],
-    optimization: {
-        cacheDir: '.manifest-cache',
-        watchMode: {
-            patterns: ['src/**/*.ts'],
-            debounceMs: 100,
-        },
+  plugins: [
+    {
+      type: 'local',
+      source: 'src/plugins/navbar/index.ts',
     },
+    {
+      type: 'local',
+      source: 'src/plugins/views/index.ts',
+    },
+  ],
+  optimization: {
+    cacheDir: '.manifest-cache',
+    watchMode: {
+      patterns: ['src/**/*.ts'],
+      debounceMs: 100,
+    },
+  },
 };
